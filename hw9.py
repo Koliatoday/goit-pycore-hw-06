2804a0b1723e9d4a2aeb01de2d33d05c57bf0ab2
ngorb <mykola_horb@jabil.com>
2025-10-17 20:32:47 +0300

    find_phone, indents fix, prints removing

--- hw9.py
+++ hw9.py
@@ -1,6 +1,5 @@
 from collections import UserDict
 
-
 class Field:
     """Base class for fields in record"""
     def __init__(self, value):
@@ -15,14 +14,14 @@ class Name(Field):
 
 
 class Phone(Field):
-  """Class for phone number in record"""
-  def __init__(self, value):
-    if not isinstance(value, str):
-      raise TypeError("Phone must be in a string format")
-    if len(value) != 10 or not value.isdigit():
-      raise ValueError("Phone must be 10 digit string format")
+    """Class for phone number in record"""
+    def __init__(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Phone must be in a string format")
+        if len(value) != 10 or not value.isdigit():
+            raise ValueError("Phone must be 10 digit string format")
 
-    self.value = value
+        self.value = value
 
 
 class Record:
@@ -35,40 +34,36 @@ class Record:
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
     def add_phone(self, phone: str):
-      phone_obj = Phone(phone)
-      self.phones.append(phone_obj)
+        phone_obj = Phone(phone)
+        self.phones.append(phone_obj)
 
     def remove_phone(self, phone: str):
-      for el in self.phones:
-        if el.value == phone:
-          self.phones.remove(el)
+        phone_obj = self.find_phone(phone)
+        if phone_obj:
+            self.phones.remove(phone_obj)
 
     def edit_phone(self, phone: str, new_phone):
-      for el in self.phones:
-        if el.value == phone:
-          el.value = new_phone
-          break
+        phone_obj = self.find_phone(phone)
+        if phone_obj:
+            phone_obj.value = new_phone
+        else:
+            self.phones.append(Phone(new_phone))
 
     def find_phone(self, phone: str):
-      for el in self.phones:
-        if el.value == phone:
-          return phone
+        for phone_obj in self.phones:
+            if phone_obj.value == phone:
+                return phone_obj
 
 
 class AddressBook(UserDict):
-  """Class for address book"""
-  def add_record(self, record):
-    self.data[record.name.value] = record
+    """Class for address book"""
+    def add_record(self, record):
+        self.data[record.name.value] = record
 
-  def find(self, name: str):
-    if name in self.data.keys():
-      return self.data[name]
-    else:
-      print(f"{name} not found in address book")
+    def find(self, name: str):
+        if name in self.data.keys():
+            return self.data[name]
 
-  def delete(self, name: str):
-    if self.find(name):
-      del self.data[name]
-    else:
-      print(f"{name} not found in address book")
-
+    def delete(self, name: str):
+        if self.find(name):
+            del self.data[name]
